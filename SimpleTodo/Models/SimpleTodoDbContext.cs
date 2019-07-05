b957a3fc6b582fd33b56f5273fe33ba4ee211699
Tun Tun Win <ttwin@galaxywave.com>
2019-07-05 17:48:34 +0630

    Init project

---
+++ SimpleTodo/Models/SimpleTodoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTodo.Models
+{
+    public class SimpleTodoDbContext : DbContext
+    {
+        public SimpleTodoDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=SimpleTodo;Integrated Security=SSPI");
+            base.OnConfiguring(
+                optionsBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SimpleTodo/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTodo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LoginName { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+    }
+}
